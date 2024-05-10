2b95a44f5fa3c636d31e412e2e283fe85340f976
ryota-ohno <91549172+ryota-ohno@users.noreply.github.com>
2024-05-10 13:19:11 +0900

    Add files via upload

--- src/step1_6_xyz_3_.py
+++ src/step1_6_xyz_3_.py
@@ -170,7 +170,7 @@ def get_opt_params_dict(df_cur, init_params_dict,fixed_params_dict):
                     if len(df_val_xyz)==0:
                         para_list.append([x2,y2,z2])
                         continue
-                    xyz2_list.append(x2,y2,z2);E_list.append(df_val_xyz['E'].values[0])
+                    xyz2_list.append([x2,y2,z2]);E_list.append(df_val_xyz['E'].values[0])
         if len(para_list) != 0:
             return False,para_list
         x2_init,y2_init,z2_init = xyz2_list[np.argmin(np.array(E_list))]
